cf1696dd81a7b135e17229158cbbae4bb5df24cd
Greg <47503020+Grzegorz-G-Drela@users.noreply.github.com>
2026-06-03 13:40:46 +0100

    Add sidebar nav

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, NavLink } from "react-router-dom"
 
 
 function Layout() {
     return (
-        <div>
-            <nav>Navbar</nav>
-            <Outlet>
+        <div className="flex h-screen">
+            <nav className="w-48">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/map'>Map</NavLink>
+                <NavLink to='/trade'>Trade</NavLink>
+            </nav>
+            <div className="flex-1">
+                <Outlet>
 
-            </Outlet>
+                </Outlet>
+            </div>
         </div>
     )
 }
