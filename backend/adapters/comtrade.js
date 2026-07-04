0d7a91357e98b2817e33ea643ab4ac5dd66579c7
Greg <47503020+Grzegorz-G-Drela@users.noreply.github.com>
2026-07-04 23:59:13 +0100

    feat: add UN Comtrade adapter maritime trade flow data

---
+++ backend/adapters/comtrade.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch';
+import 'dotenv/config';
+
+async function fetchTradeFlows(reporterCode, period) {
+    const url = `https://comtradeapi.un.org/data/v1/get/C/A/HS?reporterCode=${reporterCode}&period=${period}&partnerCode=0&cmdCode=TOTAL&flowCode=M,X&motCode=2100`;
+
+    const response = await fetch(url, {
+        headers: {
+            'Ocp-Apim-Subscription-Key': process.env.COMTRADE_API_KEY
+        }
+    });
+    const data = await response.json();
+
+    return data;
+}
+
+export { fetchTradeFlows };
+
+console.log('Key loaded:', process.env.COMTRADE_API_KEY ? 'yes' : 'no');
+
+fetchTradeFlows(826, 2023).then(data => {
+    console.log(JSON.stringify(data, null, 2));
+});
