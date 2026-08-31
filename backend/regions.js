02b79402e257db1a8a058e0891bbbcb73f3ca759
Greg <47503020+Grzegorz-G-Drela@users.noreply.github.com>
2026-08-31 23:44:29 +0100

    feat: add precise polygon shape for English Channel region

--- backend/regions.js
+++ backend/regions.js
@@ -2,25 +2,36 @@ export const REGIONS = {
     'english-channel': {
         name: 'English Channel',
         boundingBox: [[48.0, -5.0], [52.0, 5.0]],
+        preciseArea: [
+            [-5.2, 50.0], [-4.5, 48.3], [-2.0, 48.6], [0.5, 49.3],
+            [1.6, 50.9], [2.5, 51.3], [3.4, 51.9], [4.0, 52.5],
+            [2.8, 52.0], [1.3, 51.6], [0.9, 51.0], [-1.0, 50.6],
+            [-5.2, 50.0],
+        ],
     },
     'malacca': {
         name: 'Singapore / Malacca Strait',
         boundingBox: [[1.0, 98.0], [6.0, 105.0]],
+        // preciseArea: [],
     },
     'hormuz': {
         name: 'Strait of Hormuz',
         boundingBox: [[25.0, 54.0], [27.5, 57.0]],
+        // preciseArea: [],
     },
     'hormuz-wide': {
         name: 'Strait of Hormuz WIDE',
         boundingBox: [[22.0, 53.0], [27.5, 61.0]],
+        // preciseArea: [],
     },
     'suez': {
         name: 'Suez Canal',
         boundingBox: [[29.5, 32.0], [2.0, 51.2]],
+        // preciseArea: [],
     },
     'dover': {
         name: 'Dover Strait',
         boundingBox: [[50.7, 1.0], [51.2, 2.0]],
+        // preciseArea: [],
     },
 }
